16bf89fbb6151dd155bbf89fbe73dacf7d9ecc86
Matheus Alexandre <alexandremhm@gmail.com>
2021-09-14 20:54:35 -0300

    Updates

---
+++ precache-manifest.1930f8979273a014d78471a328370f47.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6df1b4326e3f7b15296cc2790ba8892",
+    "url": "/appalmirinha/index.html"
+  },
+  {
+    "revision": "d3fd018905ddb686656b",
+    "url": "/appalmirinha/static/css/2.74662cff.chunk.css"
+  },
+  {
+    "revision": "b93d21cd43048dff854f",
+    "url": "/appalmirinha/static/css/main.992129c9.chunk.css"
+  },
+  {
+    "revision": "d3fd018905ddb686656b",
+    "url": "/appalmirinha/static/js/2.19f7f70e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/appalmirinha/static/js/2.19f7f70e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b93d21cd43048dff854f",
+    "url": "/appalmirinha/static/js/main.fcf89369.chunk.js"
+  },
+  {
+    "revision": "e8da660508d99aa52972",
+    "url": "/appalmirinha/static/js/runtime-main.aa974ff7.js"
+  },
+  {
+    "revision": "b89133460c9aef111d6e773396b5917d",
+    "url": "/appalmirinha/static/media/blackHeartIcon.b8913346.svg"
+  },
+  {
+    "revision": "f87c47c37c0c0530fe7307af260caa25",
+    "url": "/appalmirinha/static/media/drinkIcon.f87c47c3.svg"
+  },
+  {
+    "revision": "157945fffa619b919f7bb65fe11b7b43",
+    "url": "/appalmirinha/static/media/exploreIcon.157945ff.svg"
+  },
+  {
+    "revision": "fdc71414f18fc48e593e45d610dfe913",
+    "url": "/appalmirinha/static/media/left-arrow-next-svgrepo-com.fdc71414.svg"
+  },
+  {
+    "revision": "b27ba19ea033355b73926443af1756e9",
+    "url": "/appalmirinha/static/media/mealIcon.b27ba19e.svg"
+  },
+  {
+    "revision": "74e487e504d5cf16d7832bef2fbacfea",
+    "url": "/appalmirinha/static/media/profileIcon.74e487e5.svg"
+  },
+  {
+    "revision": "db81213596cb3beb56440bb39aa70c8c",
+    "url": "/appalmirinha/static/media/searchIcon.db812135.svg"
+  },
+  {
+    "revision": "06215ed4cae23455c02abc804445ba74",
+    "url": "/appalmirinha/static/media/shareIcon.06215ed4.svg"
+  },
+  {
+    "revision": "f96af36da4e16e22abd3f33d3dad8651",
+    "url": "/appalmirinha/static/media/trybe_cook.f96af36d.svg"
+  },
+  {
+    "revision": "ea3b6ba893ba3908dd84f6170b75545e",
+    "url": "/appalmirinha/static/media/whiteHeartIcon.ea3b6ba8.svg"
+  }
+]);
